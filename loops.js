487cc018bcb6c64d0f6622d1c91794a241debfed
dwolfe1987 <dwolfe1987@gmail.com>
2018-09-23 23:13:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -22,7 +22,7 @@ function whileLoop (n) {
 function doWhileLoop(array) {
   do {array.pop()
   }
-    while (array.length > 0 && incrementVariable('false'))
+    while (array.length > 0 && incrementVariable())
   return array
 }
 
